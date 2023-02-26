b98113b0e3fe0bd0ff82cfa29a285d2bb7b27f84
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2023-02-26 19:59:49 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (17.16%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,6 +13,9 @@
  *     }
  * }
  */
+
+// Approach 1
+/*
 class Solution {
     
     public int height(TreeNode root){
@@ -43,3 +46,55 @@ class Solution {
         return Math.max(maxdia, Math.max(maxDiaOfLeft, maxDiaOfRight));
     }
 }
+*/
+
+// Approach 2
+
+
+
+class Solution {
+    
+    static class Pair{
+		int dia;
+		int height;
+
+		Pair(){
+			dia=0;
+			height=0;
+		}
+
+		Pair(int h , int d){
+			this.height = h;
+			this.dia = d;
+		}
+	}
+    
+    public static Pair diameter_(TreeNode root) {
+        // Your code here
+
+		if(root==null){
+			return new Pair();
+		}
+
+		Pair leftDia = diameter_(root.left);
+		Pair rightDia = diameter_(root.right);
+
+		int diaroots = leftDia.height + rightDia.height;
+		int myHeight = Math.max(leftDia.height , rightDia.height)+1;
+
+		int myDia = Math.max(diaroots, Math.max(leftDia.dia, rightDia.dia));
+
+		return new Pair(myHeight,myDia);
+
+	}
+
+
+   
+    public static int diameterOfBinaryTree(TreeNode root) {
+        Pair ans = diameter_(root);
+		return ans.dia;
+        
+       
+    }
+}
+
