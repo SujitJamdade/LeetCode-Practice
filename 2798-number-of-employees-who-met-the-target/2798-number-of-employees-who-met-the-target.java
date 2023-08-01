34ec36c5d3f21e23984f304dc39971649210f8eb
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2023-08-01 23:58:33 +0530

    Time: 1 ms (32.78%), Space: 41.1 MB (79.31%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        
+        int n = hours.length;
+        int count =0;
+        
+        for(int i=0; i<n;i++){
+            
+            if(hours[i]>=target){
+                count = count+1;
+            }
+            
+        }
+        
+        return count;
+        
+    }
+}
