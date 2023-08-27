3cc4d66937ad901603196f82f93f9ed33d41e8df
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2023-08-27 10:03:50 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (50.00%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
@@ -7,20 +7,27 @@ class Solution {
         int unknown=0;
         for(int i =0;i<moves.length();i++){
             char c = moves.charAt(i);
+            
+            // Move towards left
             if(c == 'L'){
                 count--;
             
             }
+            
+            //move towards right
             else if (c == 'R'){
                 count++;
 
             }
+            
+            // Not sure where to move, increase unknown moves
             else{
                 unknown ++;
             }
             
         }
         
+        // check how much furthest without unknown take abs and add unknown count so we can get correct furthest
         return Math.abs(count)+unknown;       
         
     }
