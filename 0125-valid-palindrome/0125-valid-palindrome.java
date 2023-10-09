95f67ded89918223489ad3cf79b9c7cfb7426f3f
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2023-10-09 23:42:30 +0530

    Time: 2 ms (99.40%), Space: 41.7 MB (89.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        // 2 Pointer Approach
+        // T= O(N) & S = O(1)
+        
+        int left = 0; 
+        int right = s.length()-1;
+        
+        while(left < right){
+            
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        
+        return true;        
+    }
+}
