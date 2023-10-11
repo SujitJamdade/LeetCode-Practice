3592d329ad30d159627f4396c6ad1e6e5bd78544
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2023-10-12 00:03:22 +0530

    Time: 4 ms (95.16%), Space: 41.9 MB (83.48%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -7,15 +7,20 @@ class Solution {
         
          for (int i = s.length() - 1; i >= 0; i--) {
              
-             if (s.charAt(i) == ' '){
+             if (s.charAt(i) == ' '){    // space set to j so in substring we can use j as last index of word
                  j = i;
              }
              
              else if (i == 0 || s.charAt(i - 1) == ' ') {
-                 if (reversed.length() != 0){
-                     reversed.append(' ');
+                 
+                 // if prev of i is space means currently we are on starting aplhabet of word
+                 
+                 if (reversed.length() != 0){    // if reversed string is empty then no need to add space 
+                     reversed.append(' ');  // this appending space will helps to maintain single space between the words
                  }
-             reversed.append(s.substring(i, j));
+                 
+                 // when we finds the start index of aplha we can append the substring from i (start) index to j (end)
+                 reversed.append(s.substring(i, j));
              }
          }
          return reversed.toString();
