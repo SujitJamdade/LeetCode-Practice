db7cbbe42a806493613085c380c0b449d48b67c4
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2024-01-19 10:23:35 +0530

    Time: 3 ms (22.54%), Space: 41.6 MB (45.38%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         
         HashMap<Integer,Integer> map = new HashMap<>();
@@ -24,5 +24,22 @@ class Solution {
         }
         return true;
         
+    }
+}
+*/
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int x : arr) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        }
+
+        Set<Integer> s = new HashSet<>();
+        for (int x : freq.values()) {
+            s.add(x);
+        }
+
+        return freq.size() == s.size();
     }
 }
