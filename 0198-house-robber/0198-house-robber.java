310e258855d5239b8ac37ab29736cdd9928628eb
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2024-01-21 19:33:36 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (56.14%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int maxRobbery = 0;
+        
+        int lastRobbery = 0;
+        int secondLastRobbery = 0;
+        
+        for(int currRobbery : nums){
+            maxRobbery = Math.max(lastRobbery, secondLastRobbery+currRobbery);
+            secondLastRobbery = lastRobbery;
+            lastRobbery = maxRobbery;
+        }
+        
+        return lastRobbery;
+    }
+}
